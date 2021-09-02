116d6ce2b4faa21e0b5dc6b8d343cc2df06c489d
MChau105 <bdmchau10005@gmail.com>
2021-09-02 14:44:36 +0700

    Chau_ remove case sensitive when upload git

---
+++ src/controller/bot.controller.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const route = express.Router();
+
+const botService = require("../service/bot.service");
+
+
+route.post("/postmessage", botService.postMessage);
+
+route.put("/addtoqueue", botService.addToQueue);
+
+route.put("/modifyerrorvideo", botService.modifyInQueueWhenVideoError);
+
+route.post("/getqueue", botService.getQueue);
+
+route.delete("/removequeue", botService.removeQueue);
+
+route.post("/gethistorymessages", botService.getHistoryMessages);
+
+
+module.exports = route;
